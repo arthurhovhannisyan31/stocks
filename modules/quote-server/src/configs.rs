b9c5cb1737ffe3e3f46012a641556d16b1291c9a
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-01-02 23:31:46 +0400

    Add server crate
    - add quotes generation struct
    - add server tcp server
    - add server health check server and monitoring
    - add quotes broadcasting to spawned threads

---
+++ modules/quote-server/src/configs.rs
@@ -0,0 +1,5 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+const SERVER_IP_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
+pub const SERVER_TCP_ADDR: SocketAddr = SocketAddr::new(SERVER_IP_ADDR, 8000);
+pub const SERVER_UPD_ADDR: SocketAddr = SocketAddr::new(SERVER_IP_ADDR, 8001);
--- modules/quote-server/src/quote.rs
+++ modules/quote-server/src/quote.rs
@@ -1,66 +1,52 @@
+use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Debug, Clone)]
-pub struct StockQuote {
-  pub ticker: String,
-  pub price: f64,
-  pub volume: u32,
-  pub timestamp: u64,
+use common::StockQuote;
+use rand::Rng;
+
+pub struct QuoteGenerator {
+  price_map: HashMap<String, f64>,
 }
-
-// Методы для сериализации/десериализации
-impl StockQuote {
-  pub fn to_string(&self) -> String {
-    format!("{}|{}|{}|{}", self.ticker, self.price, self.volume, self.timestamp)
-  }
-
-  pub fn from_string(s: &str) -> Option<Self> {
-    let parts: Vec<&str> = s.split('|').collect();
-    if parts.len() == 4 {
-      Some(StockQuote {
-        ticker: parts[0].to_string(),
-        price: parts[1].parse().ok()?,
-        volume: parts[2].parse().ok()?,
-        timestamp: parts[3].parse().ok()?,
-      })
-    } else {
-      None
-    }
-  }
-
-  // Или бинарная сериализация
-  pub fn to_bytes(&self) -> Vec<u8> {
-    let mut bytes = Vec::new();
-    bytes.extend_from_slice(self.ticker.as_bytes());
-    bytes.push(b'|');
-    bytes.extend_from_slice(self.price.to_string().as_bytes());
-    bytes.push(b'|');
-    bytes.extend_from_slice(self.volume.to_string().as_bytes());
-    bytes.push(b'|');
-    bytes.extend_from_slice(self.timestamp.to_string().as_bytes());
-    bytes
-  }
-}
-
-struct QuoteGenerator;
 
 impl QuoteGenerator {
-  pub fn generate_quote(&mut self, ticker: &str) -> Option<StockQuote> {
-    // ... логика изменения цены ...
+  pub fn new(tickers: &Vec<String>) -> Self {
+    Self {
+      price_map: tickers.iter().map(|val| (val.clone(), 1.0)).collect(),
+    }
+  }
+  pub fn generate_quote(&self, ticker: &str) -> StockQuote {
+    let last_price = self.price_map.get(ticker).unwrap_or(&1.0);
 
     let volume = match ticker {
-      // Популярные акции имеют больший объём
-      "AAPL" | "MSFT" | "TSLA" => 1000 + (rand::random::<f64>() * 5000.0) as u32,
-      // Обычные акции - средний объём
+      "AAPL" | "GOOGL" | "MSFT" | "TSLA" | "NVDA" => {
+        1000 + (rand::random::<f64>() * 5000.0) as u32
+      }
       _ => 100 + (rand::random::<f64>() * 1000.0) as u32,
     };
 
-    Some(StockQuote {
+    StockQuote {
       ticker: ticker.to_string(),
-      // price: *last_price,
-      price: 0.0,
+      price: *last_price,
       volume,
-      timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64,
-    })
+      timestamp: SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64,
+    }
+  }
+  pub fn shuffle_prices(&mut self) {
+    let mut rng = rand::rng();
+    let ratio = rng.random_range(0.5..1.5);
+
+    for (_, val) in self.price_map.iter_mut() {
+      *val *= ratio;
+    }
+  }
+  pub fn generate_quote_list(&self) -> Vec<StockQuote> {
+    self
+      .price_map
+      .iter()
+      .map(|(key, _)| self.generate_quote(key))
+      .collect()
   }
 }
